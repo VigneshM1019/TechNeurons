de235018ad92b709bd34e571ed6394f41785d8c9
VigneshM1019 <160273077+VigneshM1019@users.noreply.github.com>
2024-02-17 01:13:21 +0530

    Add files via upload

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using TechNeuronsProj.Models.Domain;
+
+
+namespace TechNeuronsProj.Controllers
+{
+    public class TaskController : Controller
+    {
+        private readonly DatabaseContext _ctx;
+        public TaskController (DatabaseContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Display ()
+        {
+            var task = _ctx.Task.ToList();
+            return View(task);
+        }
+        public IActionResult Create () 
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Models.Domain.Task task)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            try
+            {
+                _ctx.Task.Add(task);
+                _ctx.SaveChanges();
+                TempData["msg"] = "Added successfully";
+                return RedirectToAction("Create");
+
+            }
+            catch (Exception ex)
+            {
+                TempData["msg"] = "Could not added!!!";
+                return View();
+            }
+        }
+        
+        public ActionResult Edit (int Id)
+        {
+            var task = _ctx.Task.Find(Id);
+            return View(task);
+        }
+        [HttpPost]
+        public IActionResult Edit(Models.Domain.Task task)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            try
+            {
+                _ctx.Task.Update(task);
+                _ctx.SaveChanges();
+                return RedirectToAction("Display");
+
+            }
+            catch (Exception ex)
+            {
+                TempData["msg"] = "Could not update!!!";
+                return View();
+            }
+
+        }
+        public ActionResult Delete (int Id)
+        {
+            try
+            {
+                var task = _ctx.Task.Find(Id);
+                if (task != null)
+                {
+                    _ctx.Task.Remove(task);
+                    _ctx.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+
+
+            }
+            return RedirectToAction("Display");
+        }
+    }
+}
---
+++ Migrations/20240216162334_start.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TechNeuronsProj.Migrations
+{
+    /// <inheritdoc />
+    public partial class start : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Task",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Task", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Task");
+        }
+    }
+}
